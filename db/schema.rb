03f6ad979ac2a14201eb1e7ca37df7c3c51d3804
enzoperce35 <enzoperce35@gmail.com>
2021-09-12 07:55:15 +0800

    modified packagings model to fix migration error in heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_070244) do
+ActiveRecord::Schema.define(version: 2021_09_09_211846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2021_09_07_070244) do
     t.float "remaining_stock"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "product"
+    t.integer "multiplier"
+    t.bigint "sale_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sale_id"], name: "index_items_on_sale_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -90,6 +99,7 @@ ActiveRecord::Schema.define(version: 2021_09_07_070244) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "sales"
   add_foreign_key "orders", "ingredients"
   add_foreign_key "orders", "packagings"
   add_foreign_key "orders", "products"
