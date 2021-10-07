7601a81b11d76fa580cc360a49f0a6523b274c3d
enzoperce35 <enzoperce35@gmail.com>
2021-10-07 19:05:57 +0800

    added edit on products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,20 @@ class ProductsController < ApplicationController
   def new
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      redirect_back(fallback_location: 'new')
+    end
+  end
+
   def create
     @product = Product.new(product_params)
 
@@ -14,7 +28,7 @@ class ProductsController < ApplicationController
   end
 
   def index
-    @products = Product.all
+    @products = Product.all.order(:name)
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
-  validates_presence_of :name, :price, :category, on: :create
-  validates_uniqueness_of :name, on: :create
+  validates_presence_of :name, :price, :category, on: [:create, :edit]
+  validates_uniqueness_of :name, on: [:create, :edit]
   has_many :orders, dependent: :destroy
   has_many :inventory_items, through: :orders
 end
