a1b4aac1a968a1087a387689cec54512c8378874
enzoperce35 <enzoperce35@gmail.com>
2021-10-09 07:32:29 +0800

    re-designed show total

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,14 @@ class HomeController < ApplicationController
   def index
     @sales_today = Sale.where("DATE(created_at) = ?", Date.today).order(created_at: :desc)
   end
+
+  def show_sale
+    @records = Record.where.not("DATE(created_at) = ?", Date.today)
+    @item_records = @records.pluck(:items)
+    @today = Record.find_by(created_at: Time.current.beginning_of_day..Time.now)
+  end
+
+  def show_record
+    @record = Record.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
 
-  get 'sales/show_total', to: 'sales#show_total', as: 'show_total'
   get 'inventories/show_chart', to: 'inventories#show_chart', as: 'chart'
+  get 'home/show_record/:id', to: 'home#show_record', as: 'show_record'
+  get 'home/show_sale', to: 'home#show_sale', as: 'show_sale'
   post 'inventories/update_stock/:id', to: 'inventories#update_stock', as: 'update_stock'
   post 'inventories/update_parent_stock/:id', to: 'inventories#update_parent_stock', as: 'update_parent'
 
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
   resources :inventories
   resources :sales, only: [:new, :index, :create, :show, :update, :destroy]
   resources :orders, only: [:edit, :update]
+  resources :home, only: [:show]
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
