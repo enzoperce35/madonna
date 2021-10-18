09be26fa3665e1f623588aec69a57fd9eb18cb24
enzoperce35 <enzoperce35@gmail.com>
2021-10-18 08:35:28 +0800

    fixed update stock caussing errorwith blank input

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -20,7 +20,7 @@ module HomeHelper
   def expected_exhaustion(item)
     return 10 if (item.current_stock <= item.remaining_stock)
     
-    legit_records = Record.where("sales > ?", 0)
+    legit_records = Record.where("sales > ?", 2000)
     store_days = legit_records.where(created_at: item.last_restock.beginning_of_day..DateTime.current).count
     consume_days = item.current_stock - item.remaining_stock
     rate_per_unit = store_days / consume_days
