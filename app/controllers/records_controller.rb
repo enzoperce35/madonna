4e9430077c7c91aabe186b9cc3410e4a1ee24511
enzoperce35 <enzoperce35@gmail.com>
2021-10-31 15:32:04 +0800

    basic records now working

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,13 @@
 class RecordsController < ApplicationController
   def index
+    user_start = params[:start]
+    user_end = params[:end]
+    
+    @begin_date = user_start.nil? ? Date.today - 14.days : user_start
+
+    @end_date = user_end.nil? ? Date.yesterday : user_end
+    
+    @records = Record.where(created_at: @begin_date..@end_date)
   end
 
   def show
--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -1,2 +1,5 @@
 module RecordsHelper
+  def readable(date)
+    date.to_date.strftime('%b %e')
+  end
 end
