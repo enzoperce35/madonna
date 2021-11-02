90995dbcb0d777360e3e9dc6244b4474fb234975
enzoperce35 <enzoperce35@gmail.com>
2021-11-02 12:23:10 +0800

    fixed date input error

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,9 +3,9 @@ class RecordsController < ApplicationController
     user_start = params[:start]
     user_end = params[:end]
     
-    @begin_date = user_start.nil? ? Date.today - 7.days : user_start
+    @begin_date = user_start.nil? ? Date.today - 7.days : user_start.to_date
 
-    @end_date = user_end.nil? ? Date.yesterday : user_end
+    @end_date = user_end.nil? ? Date.yesterday : user_end.to_date
     
     @records = Record.where(created_at: @begin_date.beginning_of_day..@end_date.end_of_day).order(:created_at)
   end
